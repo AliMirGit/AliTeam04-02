2a329b84167934921bdb090101aaebcda4a57674
Ali Mir <ali.mir@test.com>
2023-11-13 15:49:43 -0600

    Change 13 - Clean up

--- src/App.js
+++ src/App.js
@@ -5,30 +5,11 @@ function App() {
   const value = 'World 123456';
   var text2='test 2';
 
-  /*
-  useEffect(() => {
-    (async function () {
-      //var text3  = await( await fetch(`/api/message`)).json();
-      text2  = await( await fetch(`/api/message`)).json();
-      //const { text } = await( await fetch(`/api/message`)).json();
-      // text2 = text3;
-      //alert('text 2: ' + text2);
-      //alert ('text 3: ' + text3);
-     // alert ('data: ' + data);
-      //setData(text);
-      setData(text2);
-    })();
-  });
-*/
-
   useEffect(() => {
     (async function () {
       var apiText = '';
       apiText  = await( await fetch(`/api/message`)).json();
-      //const { text } = await( await fetch(`/api/message`)).json();
-      //console.log('text: ' + text);
       console.log('apiText: ' + apiText);
-      //setData(text);
       setData(apiText);
     })();
   },[
