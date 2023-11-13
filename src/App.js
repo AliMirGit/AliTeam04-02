370a9061f525d25e3f536a4fd1e32cd5d91c4183
Ali Mir <ali.mir@test.com>
2023-11-13 12:14:56 -0600

    change 02

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`http://localhost:7071/api/message`)).json();
+      setData(text);
+    })();
+  });
+
+  return <div>123{data}</div>;
+}
+
+export default App;
+
+
+/*
+
 import React from 'react';
 
 function App() {
@@ -6,3 +26,5 @@ function App() {
 }
 
 export default App;
+
+*/
